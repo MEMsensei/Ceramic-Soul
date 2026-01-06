0f3c95bbf04c3e2d72416542197b5ede9d783ed4
Andriy <andriibabi6@gmail.com>
2026-01-07 01:10:45 +0200

    smth

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,42 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "/src/sass/style.scss";
+
+
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    slidesPerView: 1,
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    
+    breakpoints: {
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 5,
+        },
+
+        1920: {
+            spaceBetween: 35,
+        }
+    },
+
+    
+
+    modules: [Navigation, Pagination],
+  });
